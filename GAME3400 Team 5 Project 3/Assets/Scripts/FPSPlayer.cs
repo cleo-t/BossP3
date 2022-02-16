ae6799f115e6484b9decb629aef2bd96b6254d07
Ryan Gehrlein <ryan.gehrlein@gmail.com>
2022-02-16 13:54:55 -0500

    Player crouches n stuff

--- GAME3400 Team 5 Project 3/Assets/Scripts/FPSPlayer.cs
+++ GAME3400 Team 5 Project 3/Assets/Scripts/FPSPlayer.cs
@@ -5,29 +5,56 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class FPSPlayer : MonoBehaviour
 {
+    public bool crouching
+    {
+        get
+        {
+            return this.isCrouching;
+        }
+        private set
+        {
+            this.isCrouching = value;
+        }
+    }
+
     [SerializeField]
     public float moveSpeed;
     [SerializeField]
+    public float crouchSpeedCoeff = 0.125f;
+    [SerializeField]
     public float turnSpeed;
+    [SerializeField]
+    public float crouchHeightCoeff = 0.5f;
+
+    private bool isCrouching;
+    private float initialYScale;
 
     private CharacterController cc;
 
     void Start()
     {
         this.cc = this.GetComponent<CharacterController>();
+        this.isCrouching = false;
+        this.initialYScale = this.transform.localScale.y;
     }
 
     private void Update()
     {
         float turnInput = this.GetTurnInput();
         this.Turn(turnInput);
+        this.isCrouching = this.GetCrouchInput();
+    }
+
+    private bool GetCrouchInput()
+    {
+        return Input.GetButton("Crouch");
     }
 
     void FixedUpdate()
     {
+        this.SetCrouching(this.isCrouching);
         Vector3 moveInput = this.GetMoveInput();
         this.Move(moveInput);
-        this.Gravity();
     }
 
     private Vector3 GetMoveInput()
@@ -56,12 +83,15 @@ public class FPSPlayer : MonoBehaviour
     private void Move(Vector3 moveInput)
     {
         Vector3 movement = (this.transform.right * moveInput.x
-            + this.transform.forward * moveInput.z) * this.moveSpeed;
+            + this.transform.forward * moveInput.z) * (this.moveSpeed * (this.isCrouching ? this.crouchSpeedCoeff : 1));
         this.cc.SimpleMove(movement * Time.fixedDeltaTime);
     }
 
-    private void Gravity()
+    private void SetCrouching(bool crouching)
     {
-
+        this.transform.localScale = new Vector3(
+            this.transform.localScale.x,
+            crouching ? this.initialYScale * this.crouchHeightCoeff : this.initialYScale,
+            this.transform.localScale.z);
     }
 }
