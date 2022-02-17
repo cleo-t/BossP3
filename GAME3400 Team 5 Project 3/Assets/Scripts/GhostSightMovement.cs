dc0a999e5b2746365eecfba846b05f65c5e70b05
Ryan Gehrlein <ryan.gehrlein@gmail.com>
2022-02-16 20:44:23 -0500

    Pots fixed

--- GAME3400 Team 5 Project 3/Assets/Scripts/GhostSightMovement.cs
+++ GAME3400 Team 5 Project 3/Assets/Scripts/GhostSightMovement.cs
@@ -22,7 +22,6 @@ public class GhostSightMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(playerSpotted);
         if (!playerSpotted)
         {
             MoveVision();
