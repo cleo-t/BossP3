fdc4863cf8fa8ab26b928d069b0639e737b5ca59
signatoro <pignataro.s@northeastern.edu>
2022-02-16 21:58:10 -0500

    merge

--- GAME3400 Team 5 Project 3/Assets/Scripts/SpookyStuff.cs
+++ GAME3400 Team 5 Project 3/Assets/Scripts/SpookyStuff.cs
@@ -62,6 +62,7 @@ public class SpookyStuff : MonoBehaviour
     {
         if (this.canTakeDamage)  
         {
+            AudioSource.PlayClipAtPoint(GhostShot, this.transform.position);
             nextDamageTime = Time.time + damageInterval; 
             ghostHealth -= 50;
 
